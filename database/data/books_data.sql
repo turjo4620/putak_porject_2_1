72d10a5255b395cb260429d1e0e34122d25e6f7c
turjo4620 <sarkerturjo2022@gmail.com>
2026-06-29 20:53:08 +0600

    first commit

---
+++ database/data/books_data.sql
@@ -0,0 +1,8 @@
+INSERT INTO books(isbn, title, edition, lan_guage, format, stock)
+
+VALUES
+	('9847016600173', 'অপেক্ষা', '32th Edition, July 2023', 'বাংলা', 'হার্ডকভার', 100)
+
+
+
+	
---
+++ database/schema/books_create.sql
@@ -0,0 +1,23 @@
+CREATE TABLE books(
+	book_id SERIAL PRIMARY KEY,
+	isbn VARCHAR(15) UNIQUE NOT NULL,
+	title VARCHAR(250) NOT NULL,
+	edition INT NOT NULL,
+	lan_guage VARCHAR(50),
+	format CHAR(20),
+	stock INT NOT NULL,
+	description VARCHAR(1000)
+	
+);
+
+-- ALTER TABLE books
+-- ALTER COLUMN edition TYPE VARCHAR(20);
+
+ALTER TABLE books
+ALTER COLUMN format TYPE CHAR(20)
+
+-- ALTER TABLE books
+-- ALTER COLUMN edition TYPE VARCHAR(50);
+
+ALTER TABLE books
+ALTER COLUMN lan_guage TYPE CHAR(20);
